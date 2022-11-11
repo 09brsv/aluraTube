ec64cab9284d894fe770102a6ad1c5f1183d8042
Bruno Batista <104792685+09brsv@users.noreply.github.com>
2022-11-11 17:46:22 -0300

    Update Search
    Add a tag div

--- src/components/Nav/components/Search.js
+++ src/components/Nav/components/Search.js
@@ -3,13 +3,13 @@ import React from "react";
 export const Search = ({ valorDoFiltro, setFiltro }) => {
 
   return (
-    <>
+    <div>
       <input
         type="text"
         onChange={(e) => setFiltro(e.target.value)}
         value={valorDoFiltro}
       />
       <button>🔎</button>
-    </>
+    <div/>
   );
 };
